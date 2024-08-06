d45e67f3520802b964c165c5c88854a9d4aa5172
Yeicor <4929005+Yeicor@users.noreply.github.com>
2024-08-06 19:38:23 +0200

    Cheaper water shader

--- island/terrain/terrain.gd
+++ island/terrain/terrain.gd
@@ -7,21 +7,23 @@ extends MeshInstance3D
 @export var _click_to_regenerate: bool = false:
 	set(new_val):
 		if Engine.is_editor_hint():
-			_regenerate()
+			_regenerate_demo()
 
 @export var my_seed: int = 42:
 	set(new_seed):
 		my_seed = new_seed
 		if Engine.is_editor_hint():
-			_regenerate()
+			_regenerate_demo()
 
 
-
-func _regenerate(): # Ignoring errors as this is an internal tool
-	print("Regenerating mesh with seed: " + str(my_seed))
+func _regenerate_demo(): # Ignoring errors as this is an internal tool
 	var game_reader: GameReader  = GameReader.open(ConfigClass.DEFAULT_GAME_PATH)
 	var first_round: GameState   = game_reader.parse_next_state()
 	var island: Island = first_round.island(false)
+	generate(island)
+
+
+func generate(island: Island):
 	var heightmap: Array = await $HeightMapGen.generate_heightmap(island, my_seed)
 	mesh = MeshGen.from_heightmap(heightmap, Vector3(0.25, 5, 0.25))
 

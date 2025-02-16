18c1c395f74026300e57b2b57882fd183c8d3016
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-02-16 20:17:50 +0100

    Players now attack

--- island/player/player.gd
+++ island/player/player.gd
@@ -21,11 +21,12 @@ func _ready() -> void:
 	new_mat.set_shader_parameter("color_from", Vector3(0.5, 0.7, 0.8))
 	new_mat.set_shader_parameter("sensitivity", 0.2)
 	mesh_instance.set_surface_override_material(0, new_mat)
-	_idle()
+	idle()
 
-func _idle():
+func idle():
 	_anim_common("Idle", true)
-	anim_dest_time = -1
+	walk_from_transform = Transform3D.IDENTITY
+	anim_dest_time = anim_from_time
 
 func walk_to(global_center: Vector2, _delta_secs: float = Settings.common_turn_secs()):
 	# TODO: Avoid some props and follow terrain shape better? Auto-generated navmesh may cause issues?
@@ -45,15 +46,17 @@ func attack(_target: Vector3, _delta_secs: float = Settings.common_turn_secs()):
 	"""Animates the attack of the center of the cell that the player is on."""
 	for hand_bone_name in ["mixamorig_LeftHand", "mixamorig_RightHand"]:
 		var lp := preload("res://island/player/lightning/lightning_plane.tscn").instantiate()
-		add_child(lp)
+		get_parent().add_child(lp)
 		lp.name = "attack_lightning_" + hand_bone_name
 		lp.start_freedom = 0.0
-		lp.end_freedom = 0.0 # TODO: 1.0
+		lp.end_freedom = 1.0
+		# TODO: Make it more visible with more lightnings? Or strong lights or some other effect...
 		lp.color = color
 		var bone_id = skeleton.find_bone(hand_bone_name)
 		attack_lightnings.append([bone_id, lp])
+	walk_from_transform = Transform3D.IDENTITY
 	target = _target
-	_anim_common("Attack", false, _delta_secs)
+	_anim_common("Attack", true, _delta_secs)
 
 var walk_from_transform: Transform3D = Transform3D.IDENTITY
 func _walk_to(destination: Vector3, _delta_secs: float = Settings.common_turn_secs()) -> void:
@@ -66,20 +69,23 @@ var _last_anim = "None"
 func _anim_common(_name: String, loop: bool, _delta_secs: float = Settings.common_turn_secs()):
 	anim_from_time = Time.get_ticks_msec() / 1000.0
 	anim_dest_time = anim_from_time + _delta_secs
+	# Log.d("Animation SET | From: {0} | Dest: {1} | Name: {2}".format([anim_from_time, anim_dest_time, _name]))
+	if loop:
+		animation_player.animation_set_next(_name, _name)
+	elif _delta_secs > 0.01:
+		animation_player.speed_scale = _delta_secs / animation_player.current_animation_length
 	if _last_anim != _name:  # To avoid resetting animation when keeping the same one
 		animation_player.play(_name, _delta_secs / 4.0)  # Switch smoothly
 		_last_anim = _name
-	if loop:
-		animation_player.animation_set_next(_name, _name)
-	else:
-		animation_player.speed_scale = _delta_secs / animation_player.current_animation_length
 
 var target: Vector3 = Vector3.INF
 var anim_from_time: float = 0
-var anim_dest_time: float = -1
+var anim_dest_time: float = anim_from_time
 func _process(_delta: float) -> void:
 	# Determine the percentage of walk completion (0 to 1+)
 	var anim_progress = (Time.get_ticks_msec() / 1000.0 - anim_from_time) / (anim_dest_time - anim_from_time)
+	if !is_inf(anim_progress) and clampf(anim_progress, 0, 1.1) != anim_progress:
+		SLog.sw("Animation progress: {0}. Time: {1} | From: {2} | Dest: {3}".format([anim_progress, Time.get_ticks_msec() / 1000.0, anim_from_time, anim_dest_time]))
 	
 	if anim_progress >= 0.0 and anim_progress < 1.0:
 		# If walking, move towards the target based on the percentage of completion
@@ -89,7 +95,7 @@ func _process(_delta: float) -> void:
 			global_position.y = IslandH.height_at_global(Vector2(global_position.x, global_position.z))
 
 		# Always smoothly rotate to face the target while walking towards it
-		var rotation_speed = PI / Settings.common_turn_secs_multiplier()
+		var rotation_speed = max(PI, PI / Settings.common_turn_secs_multiplier())
 		var rotation_target = -Vector3(target.x - global_position.x, 0.0, target.z - global_position.z)
 		if(rotation_target.length_squared() > 0.001):
 			quaternion = lerp(quaternion, Basis.looking_at(rotation_target).get_rotation_quaternion(), _delta * rotation_speed)
@@ -97,21 +103,21 @@ func _process(_delta: float) -> void:
 		# If attacking, make sure the lightning is always aligned with the hands and target (even while rotating)
 		if not attack_lightnings.is_empty():
 			for alp in attack_lightnings:
-				var bone_pos := skeleton.global_transform # * skeleton.get_bone_global_pose(alp[0])
+				var lightning_from := skeleton.global_transform * Vector3(0.0, 0.3, -1.6) # HACK!
 				var lp = alp[1]
-				lp.set_endpoints(bone_pos.origin, target)
+				lp.set_endpoints(lightning_from, target)
 		
 	else: # Not animating
 		if walk_from_transform != Transform3D.IDENTITY:
 			if global_position != target: # End of walking event
 				walk_from_transform = Transform3D.IDENTITY
 				global_position = target
-				_idle()
+				idle()
 		
 		if not attack_lightnings.is_empty(): # End of attacking event
 			for alp in attack_lightnings:
 				var lp = alp[1]
-				remove_child(lp)
+				get_parent().remove_child(lp)
 			attack_lightnings = []
-			_idle()
+			idle()
 		
--- island/players.gd
+++ island/players.gd
@@ -25,10 +25,13 @@ func _on_terrain_terrain_ready(mi: MeshInstance3D, game: GameState) -> void:
 	GameManager.resume()
 
 
-var player_to_next_pos: Dictionary = {}  # Precomputed during init, start animation on next phase
+var _player_to_next_pos: Dictionary = {}  # Precomputed during init, start animation on next phase
+var _player_attacks: Dictionary = {}
+var _last_lighthouses_by_cell: Dictionary = {}
 func _on_game_state(state: GameState, _turn: int, phase: int):
 	if phase == SignalBus.GAME_STATE_PHASE_INIT:
-		player_to_next_pos.clear()
+		# => PRECOMPUTE MOVEMENT DATA
+		_player_to_next_pos.clear()
 		var state_players := state.players()
 		# First pass: fill the following collision dictionary
 		var cell_to_players: Dictionary = {}
@@ -49,23 +52,52 @@ func _on_game_state(state: GameState, _turn: int, phase: int):
 				var player = players_in_cell[player_index_in_cell]
 				var actual_target_cell := Vector2(cell) + Vector2i.ONE * 0.5
 				if len(players_in_cell) > 1 or cell_has_lighthouse:
+					# TODO: Player 0 prefers the side of the camera to make animations more noticeable
 					actual_target_cell = actual_target_cell + 1.0/3.0 * Vector2(
 						cos(float(player_index_in_cell) / len(players_in_cell) * PI * 2),
 						sin(float(player_index_in_cell) / len(players_in_cell) * PI * 2))
-				player_to_next_pos[player] = IslandH.cell_to_global(actual_target_cell)
+				var prev_pos = Vector2(player.position.x, player.position.z)
+				if Vector2i(IslandH.global_to_cell(prev_pos)) != Vector2i(cell): # Only move when needed! (not 100% valid in case of idles, but good enough)
+					_player_to_next_pos[player] = IslandH.cell_to_global(actual_target_cell)
+		
+		# => PRECOMPUTE ATTACK DATA
+		if not _last_lighthouses_by_cell.is_empty():
+			_player_attacks.clear()
+			for cell in cell_to_players: # "Optimization": only check lighthouses with players
+				for lh_meta in state.lighthouses():
+					if lh_meta.pos() == cell and _last_lighthouses_by_cell.has(cell):
+						var lh_prev_meta: Lighthouse = _last_lighthouses_by_cell.get(cell)
+						var expected_energy = max(0, lh_prev_meta.energy() - 10)
+						var got_energy = lh_meta.energy()
+						Log.d("Possible attack on {lh_pos}:\nPREV: owner: {prev_owner}\tenergy: {prev_energy}\nCURR: owner: {curr_owner}\tenergy: {curr_energy}".format({"lh_pos": lh_meta.pos(), "prev_owner": lh_prev_meta.owner(), "prev_energy": lh_prev_meta.energy(), "curr_owner": lh_meta.owner(), "curr_energy": lh_meta.energy()}))
+						# The game format is lossy, so we make some assumptions here...
+						if got_energy != expected_energy: # Attacked by unknown players, assume all that are not doing other stuff
+							_player_attacks[cell] = cell_to_players[cell]
+		# Store the last available lighthouse metadata for future comparison
+		_last_lighthouses_by_cell.clear()
+		for lh_meta in state.lighthouses():
+			_last_lighthouses_by_cell[lh_meta.pos()] = lh_meta
+		
 
 	elif phase == SignalBus.GAME_STATE_PHASE_ANIMATE:
 		# Apply precomputed collision-avoiding target walk locations!
-		for player in player_to_next_pos:
-			var target_pos = player_to_next_pos[player]
-			if target_pos != Vector2(player.position.x, player.position.y):
+		var players := state.players()
+		for player_index in range(players.size()):
+			var player_meta = players[player_index]
+			var player: PlayerScene = get_child(player_index)
+			var target_pos = _player_to_next_pos.get(player)
+			if target_pos != null:
 				player.walk_to(target_pos)
-			#elif attack!
+			elif _player_attacks.has(player_meta.pos()):
+				var attack_pos = IslandH.hit_pos_at_cell(Vector2(player_meta.pos()) + Vector2.ONE * 0.5)
+				attack_pos.y += 16  # TODO: Get height of lighthouse programmatically
+				player.attack(attack_pos)
+			# elif connect (special case of attack that shares animation?)
 			else:
 				player.idle()
 				
 	else: # SignalBus.GAME_STATE_PHASE_END
-		for player in player_to_next_pos:
-			var target_pos = player_to_next_pos[player]
+		for player in _player_to_next_pos:
+			var target_pos = _player_to_next_pos[player]
 			if target_pos != Vector2(player.position.x, player.position.y):
 				player.set_pos(target_pos) # Force it (shouldn't be necessary...)
